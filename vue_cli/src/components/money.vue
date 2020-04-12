490c51f6746dd2c656179f6a7c973d18e30b4639
sohep alslamat <sohep.alslamat91@gmail.com>
2020-04-12 21:44:57 +0300

    push router

--- vue_cli/src/components/money.vue
+++ vue_cli/src/components/money.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
       <h1>أخبار الاقتصاد</h1>
-      <h2>{{$route.query.username}}</h2>
-      <h2>{{$route.query.lastname}}</h2>
   </div>
 </template>
 
 <script>
 export default {
-
+  created(){
+    this.$router.push({name: 'homepage'})
+  }
+  
 }
 </script>
 
