fc70073f516a62834008bb42bfc4c4cccc5f6131
Vuango Simply Learn <sohep.alslamat91@gmail.com>
2022-08-28 00:06:14 +0300

    add vue 3 app

---
+++ vue-3/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="app">
+    <h3>My root</h3>
+    <SInput v-model="x" />
+    <p>{{x}}</p>
+    <button @click="x='Hi'">say Hi</button>
+  </main>
+</template>
+
+<script>
+import SInput from "./components/SInput.vue";
+export default {
+  components: {
+    SInput,
+  },
+  data(){
+    return{
+      x: 'hi'
+    }
+  }
+};
+</script>
+<style scoped>
+  .app {
+  height: fit-content;
+  width: fit-content;
+  padding: 10px;
+  border: 1px solid rgb(0, 255, 106);
+     
+}
+</style>
